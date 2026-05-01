b1ac8560a622f8fff8248d99f887fff402d14ae9
Costa-Chilala-dev <mr.cleaverfox@gmail.com>
2026-05-01 18:44:35 +0100

    update

--- src/controllers/RecipeController.php
+++ src/controllers/RecipeController.php
@@ -2,6 +2,11 @@
     if (session_status() === PHP_SESSION_NONE) {
         session_start();
     }
+
+    if (!isset($_SESSION['id'])) {
+        header("Location: login.html");
+        exit();
+    }
     
     $userId = $_SESSION["id"];
 
