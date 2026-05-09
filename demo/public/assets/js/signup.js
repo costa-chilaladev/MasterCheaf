6a1038bcc56fbb96460619856493c8328869ab50
Costa-Chilala-dev <mr.cleaverfox@gmail.com>
2026-05-09 01:28:46 +0100

    relative paths fixed

--- demo/public/assets/js/signup.js
+++ demo/public/assets/js/signup.js
@@ -1,5 +1,3 @@
-import { validateEmail, validatePassword } from '../../src/assets/js/utils/auth.js';
-
 const signupButton = document.getElementById('signupButton');
 
 signupButton.addEventListener('click', async (e) => {
